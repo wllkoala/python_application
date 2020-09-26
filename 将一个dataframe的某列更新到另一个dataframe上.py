0bb2806ceccbf5d6fbc99aa4b7b3616532efbd02
Simon <wllkoala@163.com>
2020-09-26 20:21:56 +0800

    Create

---
+++ 将一个dataframe的某列更新到另一个dataframe上.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df1 = pd.DataFrame({
+    'ix': [10, 22, 36, 49, 57],
+    'b': ['a', 'b', 'c', None, 'd']
+})
+df2 = pd.DataFrame({'ix': [10, 22, 57], 'b': ['e', None, 'f']})
+a = dict(zip(df1['ix'], df1['b']))
+b = dict(zip(df2['ix'], df2['b']))
+tmp_d = {}
+tmp_d.update(a)
+tmp_d.update(b)
+df1 = pd.DataFrame(tmp_d.items()).rename(columns={0: 'ix', 1: 'b'})
+print(df1)
